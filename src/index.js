92989c92e8a58d62b8cdaa28f30b3b1780986785
Marianna <31858221+Kochankin@users.noreply.github.com>
2018-02-25 23:22:58 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
-}
+  
+    var a= Number(equation.slice(0, equation.indexOf(" * x^2")).replace(" ",""));
+    var b= Number(equation.slice(equation.indexOf(" * x^2 ")+7, equation.indexOf(" * x ")).replace(" ",""));
+    var c= Number(equation.slice(equation.indexOf(" * x ")+5, equation.length).replace(" ",""));
+  
+    var d = b*b - 4*a*c;
+  
+    var roots = [];
+      var x1 = Math.round((-b + Math.sqrt(d)) / (2*a));
+      var x2 = Math.round((-b - Math.sqrt(d)) / (2*a));
+      roots.push(x1, x2);
+      return roots.sort(function(x, y) { 
+        return x - y;
+      });
+  }
+  
